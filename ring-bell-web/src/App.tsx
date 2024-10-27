75e257c157ac3af9976fa176191f7b49b3cc3624
Saksham Bhatt <sakshambhatt33@gmail.com>
2024-10-27 19:30:29 +0530

    feat: added UI & logic for ring bell to web client

--- ring-bell-web/src/App.tsx
+++ ring-bell-web/src/App.tsx
@@ -1,12 +1,59 @@
+import { useState } from "react";
+import axios from "axios";
+import LoadingSpinner from "./assets/loading-spinner.svg?react";
 import "./App.css";
 
 function App() {
-  const handleBellPress = () => {};
+  const defaultBellStatus = {
+    isLoading: false,
+    isSuccess: false,
+    isError: false,
+  };
+
+  const [bellStatus, setBellStatus] = useState(defaultBellStatus);
+
+  const handleBellPress = async () => {
+    try {
+      setBellStatus((prev) => ({ ...prev, isLoading: true }));
+
+      const firebaseEndpoint = import.meta.env.VITE_FIREBASE_ENDPOINT;
+      const res = await axios.post(`${firebaseEndpoint}/visit`);
+
+      setBellStatus((prev) => ({
+        ...prev,
+        isSuccess: true,
+        isError: false,
+      }));
+      console.log(res);
+    } catch (error) {
+      console.log({ error });
+      setBellStatus((prev) => ({
+        ...prev,
+        isSuccess: false,
+        isError: true,
+      }));
+    } finally {
+      setBellStatus((prev) => ({
+        ...prev,
+        isLoading: false,
+      }));
+    }
+  };
 
   return (
     <>
-      <h1>Press the below button to ring the bell of the house</h1>
-      <button onClick={handleBellPress}>Press me</button>
+      <h1>Press to ring bell</h1>
+
+      <p>
+        {bellStatus.isSuccess ? "Ding dong! Door will open soon..." : null}
+        {bellStatus.isError
+          ? "Bell failed! Please press button again to ring bell..."
+          : null}
+      </p>
+
+      <button onClick={handleBellPress}>
+        Press me {bellStatus.isLoading ? <LoadingSpinner /> : null}
+      </button>
     </>
   );
 }
--- ring-bell-web/src/constants/general.ts
+++
@@ -1,3 +0,0 @@
-const FIREBASE_PROJECT_ID = "ring-a-bell-460e5";
-
-export { FIREBASE_PROJECT_ID };
